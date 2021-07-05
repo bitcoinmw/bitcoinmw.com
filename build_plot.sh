a96dc75566077c24954108630158480c2c0e817b
Snape 479 <Severus_Snape93@protonmail.com>
2021-07-05 07:37:20 -0700

    change name of plot

--- build_plot.sh
+++ build_plot.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd /var/www/html/ci_uploads
-rm -rf plot.tsv
+rm -rf bmw_wallet.tsv
 for d in ./bmw_wallet/*
 do
  echo "Processing $d"
@@ -13,15 +13,15 @@ do
    if [[ "$p" == *"lines covered"* ]]; then
     #echo "$p"
     VALUE=`echo $p | cut -f1 -d '%'`;
-    echo "$TS   $VALUE" >> plot.tsv;
+    echo "$TS   $VALUE" >> bmw_wallet.tsv;
    fi
   done <$f
  done
 done
 
-gnuplot -e "set terminal svg; plot 'plot.tsv'" > bmw_wallet_cc.svg
+gnuplot -e "set terminal svg; plot 'bmw_wallet.tsv'" > bmw_wallet_cc.svg
 
-rm -rf plot.tsv
+rm -rf bitcoinmw.tsv
 for d in ./bitcoinmw/*
 do
  echo "Processing $d"
@@ -34,10 +34,10 @@ do
    if [[ "$p" == *"lines covered"* ]]; then
     #echo "$p"
     VALUE=`echo $p | cut -f1 -d '%'`;
-    echo "$TS   $VALUE" >> plot.tsv;
+    echo "$TS   $VALUE" >> bitcoinmw.tsv;
    fi
   done <$f
  done
 done
 
-gnuplot -e "set terminal svg; plot 'plot.tsv'" > bitcoinmw_cc.svg
+gnuplot -e "set terminal svg; plot 'bitcoinmw.tsv'" > bitcoinmw_cc.svg
