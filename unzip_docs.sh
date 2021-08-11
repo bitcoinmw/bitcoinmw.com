453012da56768c31f55a0f23940f316a36e7bf63
Snape 479 <Severus_Snape93@protonmail.com>
2021-08-10 22:25:16 -0700

    add unzip docs

---
+++ unzip_docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /var/www/html/ci_uploads
+for d in ./nioruntime/*
+do
+ echo "Processing $d"
+ for f in $d/doc.tar.gz
+ do
+  echo "file: $f"
+  gzip -dc $f | tar xvf -
+ done
+done
